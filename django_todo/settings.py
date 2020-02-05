3e1884879e1b1dafb65e1bf0e3d178c289930cdc
Claudia De Beer <claudiadiebeer@gmail.com>
2020-02-05 20:47:29 +0000

    set localhost

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
 
-ALLOWED_HOSTS = [os.environ.get('HOSTNAME')]
+ALLOWED_HOSTS = [os.environ.get('HOSTNAME', 'localhost')]
 
 host = os.environ.get('SITE_HOST')
 if host:
